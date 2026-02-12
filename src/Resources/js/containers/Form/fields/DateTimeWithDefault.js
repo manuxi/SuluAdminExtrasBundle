9f248ea7d0c0243aac1e37b8cba1ca4af215a0bd
Manuel Bertrams <manuel@bertrams-media.de>
2026-02-12 16:47:24 +0100

    Save fix

--- src/Resources/js/containers/Form/fields/DateTimeWithDefault.js
+++ src/Resources/js/containers/Form/fields/DateTimeWithDefault.js
@@ -1,6 +1,8 @@
 // @flow
 import React from 'react';
+import { computed } from 'mobx';
 import { observer } from 'mobx-react';
+import moment from 'moment';
 import { DatePicker } from 'sulu-admin-bundle/components';
 import type { FieldTypeProps } from 'sulu-admin-bundle/types';
 
@@ -17,19 +19,26 @@ class DateTimeWithDefault extends React.Component<FieldTypeProps<string>> {
 
     handleChange = (value: ?Date) => {
         const { onChange, onFinish } = this.props;
-        onChange(value);
+        const stringValue = value ? moment(value).format('YYYY-MM-DDTHH:mm:ss') : undefined;
+        onChange(stringValue);
         onFinish();
     };
 
     render() {
         const { dataPath, error, value, schemaOptions } = this.props;
         const defaultValue = schemaOptions?.default_value?.value;
-        const currentValue = value ? new Date(value) : (defaultValue ? new Date(defaultValue) : undefined);
+
+        let dateValue = undefined;
+        if (value) {
+            dateValue = moment(value).toDate();
+        } else if (defaultValue) {
+            dateValue = moment(defaultValue).toDate();
+        }
 
         return (
             <DatePicker
                 id={dataPath}
-                value={currentValue}
+                value={dateValue}
                 onChange={this.handleChange}
                 valid={!error}
                 options={{
